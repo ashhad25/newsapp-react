5e6da52cd41162950f4773b89df87f7de669bd11
Ashhad Ahmed <ashhadahmed72@gmail.com>
2022-07-03 00:38:09 +0500

    added infinite-scroll feature
    removed previous & next features and added a infinite scroll.

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
 
@@ -21,7 +22,8 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      totalResults: 0
     }
     document.title = `NewsApp- ${this.capitalizeFirstLetter(this.props.category)} News`;
   }
@@ -52,23 +54,39 @@ export class News extends Component {
     this.setState({page: this.state.page + 1});
     this.updateNews();
   }
+  fetchMoreData = async() => {
+    this.setState({
+      page: this.state.page + 1,
+    })
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8bdc3d4c907f4872bb69f8371d15bcd2&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles: this.state.articles.concat(parsedData.articles), totalResults: parsedData.totalResults})
+  };
+
   render() {
     return (
-      <div className='container my-3'>
+      <>
         <h1 className="text-center" style={{margin: '35px 0px'}}>{this.capitalizeFirstLetter(this.props.category)} - Top Headlines</h1>
         {this.state.loading && <Spinner/>}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+        <div className="container">
         <div className="row">
-        {!this.state.loading && this.state.articles.map((element)=>{
+        {this.state.articles.map((element)=>{
           return <div className="col-md-4" key={element.url}>
           <NewsItem title ={element.title!=null?element.title.slice(0, 45):element.title} description={element.description!=null?element.description.slice(0, 95):element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
         </div>
         })}
         </div>
-        <div className="container d-flex justify-content-between">
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.prevPage}>&#8249; Previous</button>
-        <button disabled={this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-dark" onClick={this.nextPage}>Next &#8250;</button>
         </div>
-      </div>
+        </InfiniteScroll>
+        
+      </>
     )
   }
 }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,8 +4,8 @@ import loading from './loading.gif'
 export class Spinner extends Component {
   render() {
     return (
-      <div className='text-center' style={{marginTop: '500px'}}>
-        <img src={loading} alt="loading" style={{height: '50px'}}/>
+      <div className='text-center'>
+        <img className='my-3' src={loading} alt="loading" style={{height: '50px'}}/>
       </div>
     )
   }
